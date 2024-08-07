d7f1abf88d44b9d628693c046a9fc271fbd7b71a
mackemo <mackenzielmoore14@gmail.com>
2024-08-07 16:08:53 -0400

    Copied over Skills section from previous portfolio

---
+++ Mack-portfolio/src/pages/Skills.jsx
@@ -0,0 +1,51 @@
+export default function Skills() {
+    return (
+        <section className="container col-md-10" id="skills">
+            <div className="row">
+                <h3 className="title fw-medium">Skills</h3>
+                <div className="d-flex flex-wrap justify-content-center">
+                    <div className="text-center m-4">
+
+                        <h5>Languages</h5>
+                        <img className="skills" src="" alt="HTML"/>
+                        <img className="skills" src="" alt="CSS"/>
+                        <img className="skills" src="" alt="Javascript"/>
+                    </div>
+                    <div className="text-center m-4">
+                
+                        <h5>Frontend</h5>
+                        <img className="skills" src="" alt="React"/>
+                        <img className="skills" src="" alt="jquery"/>
+                        <img className="skills" src="" alt="Bootstrap"/>
+                        <img className="skills" src="." alt="Handlebars"/>
+                    </div>
+                    <div className="text-center m-4">
+                      
+                        <h5>Backend</h5>
+                        <img className="skills" src="" alt="Node"/>
+                        <img className="skills" src="" alt="Express"/>
+                        <img className="skills" src="" alt="MVC"/>
+                        <img className="skills" src="" alt="Sequelize"/>
+                    </div>
+                    <div className="text-center m-4">
+                       
+                        <h5>Databases</h5>
+                        <img className="skills" src="" alt="MongoDB"/>
+                        <img className="skills" src="" alt="MySQL"/>
+                        <img className="skills" src="" alt="PostgreSQL"/>
+                        <img className="skills" src="" alt="NoSQL"/>
+                    </div>
+                    <div className="text-center m-4">
+                       
+                        <h5>Other</h5>
+                        <img className="skills" src="" alt="API"/>
+                        <img className="skills" src="" alt="PWA"/>
+                        <img className="skills" src="" alt="JSON"/>
+                        <img className="skills" src="" alt="VScode"/>
+                        <img className="skills" src="" alt="Agile"/>
+                    </div>
+                </div>
+            </div>
+    </section>
+    );
+} 
