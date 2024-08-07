77aa4ccae092d50ce7f6b634e4f128ed7b3a1f6b
mackemo <mackenzielmoore14@gmail.com>
2024-08-07 16:12:07 -0400

    Copied over Projects section from previous portfolio

---
+++ Mack-portfolio/src/pages/Projects.jsx
@@ -0,0 +1,92 @@
+export default function Projects() {
+    return (
+        <section className="container col-md-10" id="projects">
+            <div className="row">
+                <div>
+                    <h3 className="title fw-medium">Projects</h3>
+                    <div className="work-topics">
+                        <div className="main-work">
+                            <a href="https://tech-blog-lkbj.onrender.com" target="_blank">
+                                <img src="" alt="Tech Blog"/>
+                                <h5>Tech Blog</h5>
+                            </a>
+                        </div>
+                        <br></br>
+                        <div className="work">
+                            <a href="https://mackemo.github.io/prework-study-guide/" target="_blank">
+                                <img src="" alt="Prework Study Guide"/>
+                                <h5>Prework Study Guide</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://mackemo.github.io/3-payroll-tracker/" target="_blank">
+                                <img src="" alt="Payroll Tracker"/>
+                                <h5>Payroll Tracker</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://mackemo.github.io/4-personal-blog/" target="_blank">
+                                <img src="" alt="Personal Blog"/>
+                                <h5>Personal Blog</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://mackemo.github.io/5-task-board/" target="_blank">
+                                <img src="" alt="Task Board"/>
+                                <h5>Task Board</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://mackemo.github.io/6-weather-dashboard/" target="_blank">
+                                <img src="" alt="Weather Dashboard"/>
+                                <h5>Weather Dashboard</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://mackemo.github.io/project-1-charge-car/" target="_blank">
+                                <img src="" alt="Project 1"/>
+                                <h5>Charge Car</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://drive.google.com/file/d/1FEylRp6MDN0mAO5NpnvG0fm5p-2HMln1/view" target="_blank">
+                                <img src="" alt="Readme Generator"/>
+                                <h5>Readme Generator</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://drive.google.com/file/d/1dL5c2ptSsVas_cwclkuP5_EEBa3kF2lc/view" target="_blank">
+                                <img src="" alt="SVG Logomaker"/>
+                                <h5>SVG Logomaker</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://nine-note-taker.onrender.com" target="_blank">
+                                <img src="" alt="Note Taker"/>
+                                <h5>Note Taker</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://drive.google.com/file/d/1xM-w4MFCEGdOS1mtH_yGVQ5vfOHO2Rl9/view" target="_blank">
+                                <img src="" alt="Employee Tracker"/>
+                                <h5>Employee Tracker</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://drive.google.com/file/d/13UVQjVMGu9k3k1hvGJfX8_18R_3cMDSi/view" target="_blank">
+                                <img src="" alt="Ecommerce Site"/>
+                                <h5>Ecommerce Site</h5>
+                            </a>
+                        </div>
+                        <div className="work">
+                            <a href="https://drive.google.com/file/d/1N4CSPskwOIT0OLARWuOWCPSxcj2mDs1A/view?usp=drive_link" target="_blank">
+                                <img src="" alt="Social Network"/>
+                                <h5>Social Network</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+} 
