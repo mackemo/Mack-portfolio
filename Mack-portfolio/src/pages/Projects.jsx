81e8092ce0194e2f99621b184a429b07b476017a
mackemo <mackenzielmoore14@gmail.com>
2024-08-07 17:24:33 -0400

    Imported images into Projects.jsx

--- Mack-portfolio/src/pages/Projects.jsx
+++ Mack-portfolio/src/pages/Projects.jsx
@@ -1,3 +1,5 @@
+import images from '../assets/images'
+
 export default function Projects() {
     return (
         <section className="container col-md-10" id="projects">
@@ -7,80 +9,80 @@ export default function Projects() {
                     <div className="work-topics">
                         <div className="main-work">
                             <a href="https://tech-blog-lkbj.onrender.com" target="_blank">
-                                <img src="" alt="Tech Blog"/>
+                                <img src={images.tech} alt="Tech Blog"/>
                                 <h5>Tech Blog</h5>
                             </a>
                         </div>
                         <br></br>
                         <div className="work">
                             <a href="https://mackemo.github.io/prework-study-guide/" target="_blank">
-                                <img src="" alt="Prework Study Guide"/>
+                                <img src={images.prework} alt="Prework Study Guide"/>
                                 <h5>Prework Study Guide</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://mackemo.github.io/3-payroll-tracker/" target="_blank">
-                                <img src="" alt="Payroll Tracker"/>
+                                <img src={images.payroll} alt="Payroll Tracker"/>
                                 <h5>Payroll Tracker</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://mackemo.github.io/4-personal-blog/" target="_blank">
-                                <img src="" alt="Personal Blog"/>
+                                <img src={images.blog} alt="Personal Blog"/>
                                 <h5>Personal Blog</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://mackemo.github.io/5-task-board/" target="_blank">
-                                <img src="" alt="Task Board"/>
+                                <img src={images.task} alt="Task Board"/>
                                 <h5>Task Board</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://mackemo.github.io/6-weather-dashboard/" target="_blank">
-                                <img src="" alt="Weather Dashboard"/>
+                                <img src={images.weather} alt="Weather Dashboard"/>
                                 <h5>Weather Dashboard</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://mackemo.github.io/project-1-charge-car/" target="_blank">
-                                <img src="" alt="Project 1"/>
+                                <img src={images.charge} alt="Project 1"/>
                                 <h5>Charge Car</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://drive.google.com/file/d/1FEylRp6MDN0mAO5NpnvG0fm5p-2HMln1/view" target="_blank">
-                                <img src="" alt="Readme Generator"/>
+                                <img src={images.readme} alt="Readme Generator"/>
                                 <h5>Readme Generator</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://drive.google.com/file/d/1dL5c2ptSsVas_cwclkuP5_EEBa3kF2lc/view" target="_blank">
-                                <img src="" alt="SVG Logomaker"/>
+                                <img src={images.svg} alt="SVG Logomaker"/>
                                 <h5>SVG Logomaker</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://nine-note-taker.onrender.com" target="_blank">
-                                <img src="" alt="Note Taker"/>
+                                <img src={images.note} alt="Note Taker"/>
                                 <h5>Note Taker</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://drive.google.com/file/d/1xM-w4MFCEGdOS1mtH_yGVQ5vfOHO2Rl9/view" target="_blank">
-                                <img src="" alt="Employee Tracker"/>
+                                <img src={images.emp} alt="Employee Tracker"/>
                                 <h5>Employee Tracker</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://drive.google.com/file/d/13UVQjVMGu9k3k1hvGJfX8_18R_3cMDSi/view" target="_blank">
-                                <img src="" alt="Ecommerce Site"/>
+                                <img src={images.ecom} alt="Ecommerce Site"/>
                                 <h5>Ecommerce Site</h5>
                             </a>
                         </div>
                         <div className="work">
                             <a href="https://drive.google.com/file/d/1N4CSPskwOIT0OLARWuOWCPSxcj2mDs1A/view?usp=drive_link" target="_blank">
-                                <img src="" alt="Social Network"/>
+                                <img src={images.social} alt="Social Network"/>
                                 <h5>Social Network</h5>
                             </a>
                         </div>
