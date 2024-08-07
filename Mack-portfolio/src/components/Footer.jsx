712765383f71a35f341392351820a7c3916f1859
mackemo <mackenzielmoore14@gmail.com>
2024-08-07 19:25:03 -0400

    Implemented footer on each page

---
+++ Mack-portfolio/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+  return (
+    <footer className="container">
+        <div className="row">
+            <div className="col-md-8">
+                <h6>Created by Mackenzie Moore</h6>
+            </div>
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
