6e4c40decac4898b431884538519c47168a273fb
mackemo <mackenzielmoore14@gmail.com>
2024-08-07 17:59:05 -0400

    Added contact form and handlechange and handlesubmit

--- Mack-portfolio/src/pages/Contact.jsx
+++ Mack-portfolio/src/pages/Contact.jsx
@@ -1,5 +1,37 @@
+import { useState } from 'react'
+
 export default function Contact() {
+    
+    // contact form
+    const [form, setForm] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+
+    // set changes of form input
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setForm({
+            ...form,
+            [name]: value,
+        });
+    };
+        
+    const handleSubmit = (e) => {
+        e.preventDefault();
+       
+        console.log('Form submitted:', form);
+        // reset form after submission
+        setForm({
+            name: '',
+            email: '',
+            message: '',
+        });
+    };
+
     return (
+        <>
         <section id="contact" className="container col-md-10">
             <h3 className="title fw-medium">Contact</h3>
             <div className="row d-flex justify-content-center">
@@ -13,5 +45,47 @@ export default function Contact() {
                 </div>  
             </div>
         </section>
+
+        <div className="contact-section">
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={form.name}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+            
+                <div className="form-group">
+                    <label htmlFor="email">Email Address</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+            
+                <div className="form-group">
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        value={form.message}
+                        onChange={handleChange}
+                        required
+                    ></textarea>
+                </div>
+            
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+        </>
     );
-} 
+};
