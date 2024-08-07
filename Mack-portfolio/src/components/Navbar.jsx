191217ad2ad7743e8347827939e64debf5e017ed
mackemo <mackenzielmoore14@gmail.com>
2024-08-07 15:55:05 -0400

    Included Navbar to components, made links to About, Skills, Projects, Contact

---
+++ Mack-portfolio/src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { Link, useLocation } from 'react-router-dom';
+
+
+function NavBar() {
+  const currentPage = useLocation().pathname;
+
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link
+          to="/"
+          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Skills"
+           className={currentPage === '/Skills' ? 'nav-link active' : 'nav-link'}
+        >
+          Skills
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Projects"
+          className={currentPage === '/Projects' ? 'nav-link active' : 'nav-link'}
+        >
+          Projects
+        </Link>
+      </li>
+      <li className="nav-item">
+        <Link
+          to="/Contact"
+          className={currentPage === '/Contact' ? 'nav-link active' : 'nav-link'}
+        >
+          Contact
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+export default NavBar;
