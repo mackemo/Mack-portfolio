4f0f30d05bd8f6304a0edc2f9a8b68bc3d730184
mackemo <mackenzielmoore14@gmail.com>
2024-08-08 12:24:27 -0400

    Updated github links and descriptions

--- Mack-portfolio/src/pages/Projects.jsx
+++ Mack-portfolio/src/pages/Projects.jsx
@@ -12,8 +12,8 @@ export default function Projects() {
                                 <img src={images.tech} alt="Tech Blog"/>
                                 <h5>Tech Blog</h5>
                                 <div className="main-info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/12-tech-blog" target="_blank">link to repo</a>
+                                    <p>Handlebars.js, Sequelize, Bootstrap</p>
                                 </div>
                             </a>
                         </div>
@@ -23,8 +23,8 @@ export default function Projects() {
                                 <img src={images.prework} alt="Prework Study Guide"/>
                                 <h5>Prework Study Guide</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/prework-study-guide" target="_blank">link to repo</a>
+                                    <p>Html, Css</p>
                                 </div>
                             </a>
                         </div>
@@ -33,8 +33,8 @@ export default function Projects() {
                                 <img src={images.payroll} alt="Payroll Tracker"/>
                                 <h5>Payroll Tracker</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/3-payroll-tracker" target="_blank">link to repo</a>
+                                    <p>Javascript</p>
                                 </div>
                             </a>
                         </div>
@@ -43,8 +43,8 @@ export default function Projects() {
                                 <img src={images.blog} alt="Personal Blog"/>
                                 <h5>Personal Blog</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/12-tech-blog" target="_blank">link to repo</a>
+                                    <p>Javascript, Css</p>
                                 </div>
                             </a>
                         </div>
@@ -53,8 +53,8 @@ export default function Projects() {
                                 <img src={images.task} alt="Task Board"/>
                                 <h5>Task Board</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/5-task-board" target="_blank">link to repo</a>
+                                    <p>jQuery, Bootstrap</p>
                                 </div>
                             </a>
                         </div>
@@ -63,8 +63,8 @@ export default function Projects() {
                                 <img src={images.weather} alt="Weather Dashboard"/>
                                 <h5>Weather Dashboard</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/6-weather-dashboard" target="_blank">link to repo</a>
+                                    <p>Server-side APIs, Bootstrap</p>
                                 </div>
                             </a>
                         </div>
@@ -73,8 +73,8 @@ export default function Projects() {
                                 <img src={images.charge} alt="Project 1"/>
                                 <h5>Charge Car</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/project-1-charge-car" target="_blank">link to repo</a>
+                                    <p>Server-side APIs, Bootstrap, Team Project</p>
                                 </div>
                             </a>
                         </div>
@@ -83,8 +83,8 @@ export default function Projects() {
                                 <img src={images.readme} alt="Readme Generator"/>
                                 <h5>Readme Generator</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/7-readme-generator" target="_blank">link to repo</a>
+                                    <p>Inquirer, Markdown</p>
                                 </div>
                             </a>
                         </div>
@@ -93,8 +93,8 @@ export default function Projects() {
                                 <img src={images.svg} alt="SVG Logomaker"/>
                                 <h5>SVG Logomaker</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/8-svg-logomaker" target="_blank">link to repo</a>
+                                    <p>Unit testing, Inquirer, SVG markdown</p>
                                 </div>
                             </a>
                         </div>
@@ -103,8 +103,8 @@ export default function Projects() {
                                 <img src={images.note} alt="Note Taker"/>
                                 <h5>Note Taker</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/9-note-taker" target="_blank">link to repo</a>
+                                    <p>Express.js</p>
                                 </div>
                             </a>
                         </div>
@@ -113,8 +113,8 @@ export default function Projects() {
                                 <img src={images.emp} alt="Employee Tracker"/>
                                 <h5>Employee Tracker</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/10-employee-tracker" target="_blank">link to repo</a>
+                                    <p>Inquirer, PostgreSQL</p>
                                 </div>
                             </a>
                         </div>
@@ -123,8 +123,8 @@ export default function Projects() {
                                 <img src={images.ecom} alt="Ecommerce Site"/>
                                 <h5>Ecommerce Site</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/11-ecommerce-site" target="_blank">link to repo</a>
+                                    <p>Sequelize, PostgreSQL, Express.js</p>
                                 </div>
                             </a>
                         </div>
@@ -133,8 +133,8 @@ export default function Projects() {
                                 <img src={images.social} alt="Social Network"/>
                                 <h5>Social Network</h5>
                                 <div className="info">
-                                    <a href="https://github.com/yourusername/tech-blog" target="_blank">GitHub</a>
-                                    <p>Blog platform to share technical insights and updates.</p>
+                                    <a href="https://github.com/mackemo/12-tech-blog" target="_blank">link to repo</a>
+                                    <p>MongoDB, Express.js</p>
                                 </div>
                             </a>
                         </div>
