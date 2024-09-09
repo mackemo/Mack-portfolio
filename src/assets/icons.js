b47e74f2a76fa24b570e74f44376d6806454e2a6
mackemo <mackenzielmoore14@gmail.com>
2024-09-09 15:42:46 -0400

    Included new images in skills

--- src/assets/icons.js
+++ src/assets/icons.js
@@ -1,11 +1,14 @@
 import agile from './icons/agile.png';
 import api from './icons/api.png';
+import aws from './icons/aws.png';
 import bootstrap from './icons/bootstrap.png';
+import c from './icons/c.png';
 import css from './icons/css.png';
 import express from './icons/express.png';
 import github from './icons/github.png';
 import handlebars from './icons/handlebars.png';
 import html from './icons/html.png';
+import java from './icons/java.png';
 import jquery from './icons/jquery.png';
 import js from './icons/js.png';
 import json from './icons/json.png';
@@ -17,13 +20,14 @@ import node from './icons/node.png';
 import nosql from './icons/nosql.png';
 import postgresql from './icons/postgresql.png';
 import pwa from './icons/pwa.png';
+import python from './icons/python.png';
 import react from './icons/react.png';
 import resume from './icons/resume.png';
 import sequelize from './icons/sequelize.png';
 import vscode from './icons/vscode.png';
 
 export default {
-    agile, api, bootstrap, css, express, github, handlebars, html,
+    agile, api, aws, bootstrap, c, css, express, github, handlebars, html, java,
     jquery, js, json, linkedin, mongo, mvc, mysql, node, nosql,
-    postgresql, pwa, react, resume, sequelize, vscode
+    postgresql, pwa, python, react, resume, sequelize, vscode
 };
