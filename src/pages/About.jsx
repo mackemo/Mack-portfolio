809a080ba093f3013ca24a3812914a8b9148e945
mackemo <mackenzielmoore14@gmail.com>
2024-12-02 19:21:20 -0600

    Adjusted spacing for new paragraph in About section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
         <section className="container col-md-10" id="about">
             <div className="row">
                 <div>
-                    <div className="d-flex flex-wrap align-items-center m-4">
+                    <div className="d-flex flex-wrap align-items-center">
                         <div className="intro col-md-6 d-flex flex-column">
                             <h3 className="name fw-normal">Hello!</h3> 
                             <h4 className="fs-3 fw-normal fst-italic">Aspiring Software Developer</h4>
@@ -15,9 +15,11 @@ export default function About() {
                                 where I earned a certificate with perfect scores on all assignments. This achievement highlights my commitment 
                                 and passion for excellence in software development!
                                 <br></br>
+                                <br></br>
                                 For some time, I worked at Flat Earth Networking based in Nashville as a SOC Analyst. I monitored security alerts using 
                                 SIEM tools like Qradar and resolved tickets according to documented procedures. Currently, I work as a Support Engineer
                                 at the same company and manage support tickets by documenting notes and implementing security rules in Panorama.
+                                <br></br>
                                 <br></br>
                                 With a Bachelor’s degree in Biology from the University of Tampa, I bring a detail-oriented approach to every task. 
                                 I am dedicated to continuously challenging myself and mastering the skills needed to effectively complete every project 
